ad213c6fd575d9fbc08342faae362f6a2487338f
Huzaifah <154003597+Sa1f27@users.noreply.github.com>
2024-08-20 21:45:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('css.html')
+    return render_template('index.html')
 
 @app.route('/save_transcription', methods=['POST'])
 def save_transcription():
